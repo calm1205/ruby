2b529c7f4b64ffc52f1771947c0052ab3b97009a
calm1205 <naginani072@gmail.com>
2024-04-24 08:47:42 +0900

    fizzbuzz

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,2 +1,19 @@
 
-if ( 3 % )
+def fizz(n)
+  "fizz" if n % 3 == 0
+end
+
+def buzz(n)
+  "buzz" if n % 5 == 0
+end
+
+def main(n)
+  return "fizzbuzz" if fizz(n) && buzz(n)
+  return fizz(n) || buzz(n) || n
+end
+
+i = 0
+while(i < 100)
+  i += 1
+  puts main(i)
+end
