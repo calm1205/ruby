f4368c7f92f937b4c963bb7d0627ba40c0abab83
calm1205 <naginani072@gmail.com>
2024-04-25 09:10:50 +0900

    mutable

---
+++ src/mutable/imutable.rb
@@ -0,0 +1,8 @@
+array = Array.new(5){ "default"}
+
+first = array[0]
+firstUpcase = first.upcase
+
+pp array[0] # "default"
+pp array # ["default", "default", "default", "default", "default"]
+pp firstUpcase # "DEFAULT"
---
+++ src/mutable/mutable.rb
@@ -0,0 +1,17 @@
+array = Array.new(5){ "default"}
+
+first = array[0]
+first.upcase!
+
+pp array[0] # "DEFAULT"
+pp array # ["DEFAULT", "default", "default", "default", "default"]
+
+# --------------------------
+
+array2 = Array.new(5, "default")
+
+first2 = array2[0]
+first2.upcase!
+
+pp array2[0] # "DEFAULT"
+pp array2 # ["DEFAULT", "DEFAULT", "DEFAULT", "DEFAULT", "DEFAULT"]
