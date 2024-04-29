923b7fb183641d04976c4f295060d0a9b45fca15
calm1205 <naginani072@gmail.com>
2024-04-29 18:07:25 +0900

    convert

--- src/regexp/convert_hash_syntax.rb
+++ src/regexp/convert_hash_syntax.rb
@@ -1,3 +1,6 @@
 def convert_hash_syntax(original_syntax)
   original_syntax.gsub(/:(?<key>\w+) *=> */, '\k<key>: ')
+  # original_syntax.gsub(/:(?<key>\w+) *=> */) do
+  #   "#{$~[:key]}: "
+  # end
 end
