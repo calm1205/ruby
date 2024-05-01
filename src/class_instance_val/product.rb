6f9cf862b02aa5f5d999cdfcc89c17409d7401da
calm1205 <naginani072@gmail.com>
2024-05-01 09:22:33 +0900

    class instance methods

---
+++ src/class_instance_val/product.rb
@@ -0,0 +1,15 @@
+class Product
+  @name = "Product" # クラスインスタンス変数（インスタンスメソッドからはアクセス出来ない）
+  @@name = "Product" # クラス変数（クラスメソッド、インスタンスメソッドからアクセス可能）
+
+  attr_accessor :name
+
+  def self.name
+    @name # クラスインスタンス変数
+  end
+
+  def initialize(name)
+    @name = name
+  end
+
+end
