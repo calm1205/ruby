25624325c4b38d98ea8095f6554077d28ccc8c20
calm1205 <naginani072@gmail.com>
2024-05-04 14:45:38 +0900

    check ancestor

--- src/error_handle/zero_division.rb
+++ src/error_handle/zero_division.rb
@@ -3,6 +3,7 @@ begin
   "hello world".foo
 rescue ZeroDivisionError => e
   puts "Error Class: #{e.class}"
+  puts "Error Ancestor: #{e.class.ancestors}"
   puts "Error Message: #{e.message}"
 
   puts "Backtrace ----- "
