f7b65b9ca135bb98ca5513214390d762a27d1203
calm1205 <naginani072@gmail.com>
2024-05-05 13:37:05 +0900

    pin

--- src/pattert_match/sample_pin.rb
+++ src/pattert_match/sample_pin.rb
@@ -8,3 +8,14 @@ puts case name
      in ^bob
        "Bobさん、こんにちは!"
      end
+
+records = [[7, 7, 7], [5, 6, 7]]
+
+records.each do |record|
+  case record
+  in [n, ^n, ^n]
+    puts "All elements are #{n}"
+  else
+    puts "Not all elements are the same"
+  end
+end
